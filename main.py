929e71ada007c1b3778cd56a6ba5c43adffe868a
tommasomazz-alt <tommaso.mazz@gmail.com>
2026-03-14 18:37:49 +0100

    creato player.py, circleshape.py, aggiornato constants.py e main.py - ora viene visualizzato il cursore (immobile) del giocatore

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    #Creo Giocatore
+    Player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    #Game Loop
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        pygame.display.flip()     
+        Player_1.draw(screen)
+        pygame.display.flip()    
+        dt = clock.tick(60) / 1000
+
 	
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}" )
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")	
+
+
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,26 @@
+import pygame
+import circleshape as cs
+from constants import PLAYER_RADIUS, LINE_WIDTH
+
+class Player(cs.CircleShape):
+
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):        
+        color = "white"
+        points_list = self.triangle()
+        pygame.draw.polygon(screen, color, points_list, LINE_WIDTH)
+
+
+
+

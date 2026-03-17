08c6d2e16d8b5d90c572a4ae1db9c1d8d82cfe9d
tommasomazz-alt <tommaso.mazz@gmail.com>
2026-03-17 23:25:57 +0100

    aggiunto meccanica shot tramite file shot.py, aggiunta costanti, aggiunta metodo shot a player.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_TURN_SPEED
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -18,10 +19,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     #Aggiungo la classe Player ai due gruppi
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
 
     #Creo AsteroidField
     Asteroid_Field = AsteroidField()
--- player.py
+++ player.py
@@ -1,6 +1,13 @@
 import pygame
 import circleshape as cs
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot
+from constants import (
+                        PLAYER_RADIUS,
+                        LINE_WIDTH,
+                        PLAYER_TURN_SPEED,
+                        PLAYER_SPEED,
+                        PLAYER_SHOOT_SPEED
+                        )
 
 class Player(cs.CircleShape):
 
@@ -37,13 +44,19 @@ class Player(cs.CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
-
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0,1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation)
+        shot.velocity *= PLAYER_SHOOT_SPEED
 
 
 
